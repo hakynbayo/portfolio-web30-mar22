3431cd0814d81379a00b295c439eb6d7070d7ca8
hakynbayo <akinbayo.akinwande@decagon.dev>
2024-07-04 03:16:13 +0100

    whatsapp

--- src/data.js
+++ src/data.js
@@ -1,7 +1,6 @@
 import {
   IoCodeWorking,
   IoLogoGithub,
-  IoLogoYoutube,
   IoLogoTwitter,
   IoLogoLinkedin,
   IoLogoWhatsapp,
@@ -20,17 +19,16 @@ export const Experience = [
     title: "THRINDLE. ",
     location: "LAGOS, NIGERIA",
     description:
-    "Contributed to the development of web pages and web-based applications using HTML, React.js, and Next.js. Collaborated closely with the design team to implement UI/UX designs for websites, ensuring a seamless and visually appealing user experience. Conducted code debugging to identify and resolve errors, enhancing the overall functionality of the applications.Actively participated in team meetings, providing valuable insights, and contributing to discussions on project progress and challenges."  
-   
+      "Contributed to the development of web pages and web-based applications using HTML, React.js, and Next.js. Collaborated closely with the design team to implement UI/UX designs for websites, ensuring a seamless and visually appealing user experience. Conducted code debugging to identify and resolve errors, enhancing the overall functionality of the applications.Actively participated in team meetings, providing valuable insights, and contributing to discussions on project progress and challenges.",
   },
   {
-    id: 2, 
+    id: 2,
     date: "2022 - Mar 2023",
     iconsSrc: <IoCodeWorking />,
     title: "ASSOCIATE FRONTEND DEVELOPER/DECAGON.",
     location: "LAGOS, NIGERIA",
     description:
-      "Built projects with Nodejs, React.js, Typescript, and Javascript. "
+      "Built projects with Nodejs, React.js, Typescript, and Javascript. ",
   },
   {
     id: 3,
@@ -41,7 +39,6 @@ export const Experience = [
     description:
       "Key individual to providing expert knowledge relating to products and services to new and existing clients. Providing customer services, to resolve and answer various banking questions.",
   },
-
 ];
 
 export const Projects = [
@@ -51,7 +48,7 @@ export const Projects = [
     imageSrc: img1,
     techs: "ReactJs,Typescript, ContextApi, React Icon, Firebase, Postgres",
     github: "https://github.com/hakynbayo/Music-App",
-    link: "https://smooze.netlify.app/"
+    link: "https://smooze.netlify.app/",
   },
   {
     id: 2,
@@ -59,7 +56,7 @@ export const Projects = [
     imageSrc: img4,
     techs: "ReactJs,Typescript, Redux, React Icon, Firebase",
     github: "https://github.com/hakynbayo/Smooze-movie",
-    link: "https://smooze-movie.netlify.app/"
+    link: "https://smooze-movie.netlify.app/",
   },
   {
     id: 3,
@@ -67,7 +64,7 @@ export const Projects = [
     imageSrc: img2,
     techs: "React Js, Sanity,TypeScript,SwiperJs, Tailwind CSS",
     github: "https://github.com/hakynbayo/Ecommerice-Website-in-React",
-    link: "https://faithy-store.netlify.app/"
+    link: "https://faithy-store.netlify.app/",
   },
   {
     id: 4,
@@ -75,7 +72,7 @@ export const Projects = [
     imageSrc: img3,
     techs: "React, Tailwind CSS, TypeScript, ExpressJs, MongoDB ",
     github: "https://github.com/hakynbayo",
-    link: "https://profiles.decagonhq.com/"
+    link: "https://profiles.decagonhq.com/",
   },
   {
     id: 5,
@@ -83,9 +80,8 @@ export const Projects = [
     imageSrc: img5,
     techs: "ReactJs, Tailwind CSS, TypeScript, React Icons ",
     github: "https://github.com/hakynbayo",
-    link: "https://acepay-t7jh.onrender.com/"
+    link: "https://acepay-t7jh.onrender.com/",
   },
-  
 ];
 
 export const SocialLinks = [
@@ -101,7 +97,7 @@ export const SocialLinks = [
       <IoLogoTwitter className="text-blue-500 text-3xl cursor-pointer" />
     ),
     name: "Twitter",
-    link: "",
+    link: "https://x.com/mistayagi",
   },
   {
     id: 1,
@@ -117,6 +113,6 @@ export const SocialLinks = [
       <IoLogoWhatsapp className="text-green-500 text-3xl cursor-pointer" />
     ),
     name: "Whatsapp",
-    link: "https://api.whatsapp.com/send?phone=+2347061511468&text&app_absent=0"
+    link: "https://wa.me/2347061511468",
   },
 ];
